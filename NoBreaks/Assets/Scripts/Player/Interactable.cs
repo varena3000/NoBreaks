dfb340edd62e8e32004ff0f6adbda0cc7c11de03
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2025-08-18 21:23:11 +0200

    Nathan
    Mechanic scene
    Interactable script
    added an outline asset

---
+++ NoBreaks/Assets/Scripts/Player/Interactable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    Outline outline;
+    public string message;
+
+    pubic UnityEvent onInteraction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        DisableOutline()
+    }
+
+    public void Interact()
+    {
+        onInteraction.Invoke();
+    }
+
+    public void DisableOutline()
+    {
+        outline.enabled = false;
+    }
+
+    pubic void EnableOutline()
+    {
+        outline.enabled = true;
+    }
+
+}
