687832181cc16dbf68f27f4287831163d57c5d8f
varena3000 <2324590@students.wits.ac.za>
2025-09-22 16:01:34 +0200

    SetUp for level 1
    A system for collecting Jenni's Parts added with appropriate prefabs

---
+++ NoBreaks/Assets/Scripts/Managers/CollectibleManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollectibleManager : MonoBehaviour
+{
+    [Tooltip("Number of items needed to trigger the event")]
+    public int itemsNeededToTrigger = 5;
+
+    private int collectedItemCount = 0;
+
+    // UnityEvent to be invoked when required number of items collected.
+    [Tooltip("Configure events to trigger once required items collected")]
+    public UnityEvent OnRequiredItemsCollected;
+
+    private void OnEnable()
+    {
+        CollectibleItem.OnItemCollected += HandleItemCollected;
+    }
+
+    private void OnDisable()
+    {
+        CollectibleItem.OnItemCollected -= HandleItemCollected;
+    }
+
+    private void HandleItemCollected()
+    {
+        collectedItemCount++;
+        Debug.Log($"Collected {collectedItemCount}/{itemsNeededToTrigger} items.");
+
+        if (collectedItemCount >= itemsNeededToTrigger)
+        {
+            Debug.Log("Collected required number of items!");
+            OnRequiredItemsCollected?.Invoke();
+
+            // Optional: Reset count if you want to trigger the event multiple times
+            // collectedItemCount = 0;
+        }
+    }
+}
