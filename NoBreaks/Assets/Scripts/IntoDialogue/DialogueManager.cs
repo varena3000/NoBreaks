5ba6cf9b0eef425dcd90469efdc518c5890c94b8
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2025-10-17 15:14:44 +0200

    Nathan --- Dialogue system
    Fixed the continue button skipping the whole dialogue

--- NoBreaks/Assets/Scripts/IntoDialogue/DialogueManager.cs
+++ NoBreaks/Assets/Scripts/IntoDialogue/DialogueManager.cs
@@ -1,37 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; // Importing Unity UI namespace for UI elements
+using UnityEngine.UI; 
 using UnityEngine.SceneManagement;
-using TMPro; // Importing Scene Management namespace for scene handling
+using TMPro; 
+
+//Title:How to make a Dialogue System in Unity
+//Author: Brackeys
+//Date: 21-08-25
+//Code Version: N/A
+//Available at: https://www.youtube.com/watch?v=_nRzoTzeyxU
 
 public class DialogueManager : MonoBehaviour
 {
-    // This script manages the dialogue system in the game.
 
-    //Title:How to make a Dialogue System in Unity
-    //Author: Brackeys
-    //Date: 21-08-25
-    //Code Version: N/A
-    //Available at: https://www.youtube.com/watch?v=_nRzoTzeyxU
+    public TextMeshProUGUI dialogueText;
 
-    
-    public TextMeshProUGUI dialogueText; // UI Text element to display the dialogue text
+    // Queue to hold the sentences of the dialogue
+    private Queue<string> sentences;
 
-    private Queue<string> sentences; // Queue to hold the sentences of the dialogue
+    private DialogueTrigger dialogueTrigger;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        dialogueTrigger = FindAnyObjectByType<DialogueTrigger>();
         sentences = new Queue<string>();
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    // This method is called to start the dialogue
+    public void StartDialogue(Dialogue dialogue)
     {
-        // This method is called to start the dialogue
-
-    
-
         sentences.Clear(); // Clear any previous sentences
 
         // Enqueue each sentence from the dialogue into the queue
@@ -44,20 +42,22 @@ public class DialogueManager : MonoBehaviour
 
     }
 
+    // This method displays the next sentence in the dialogue
     public void DisplayNextSentence()
     {
-        // This method displays the next sentence in the dialogue
-        if (sentences.Count == 0)
+        if (dialogueTrigger.istriggered == true && sentences.Count == 0)
         {
             EndDialogue(); // If no more sentences, end the dialogue
             return;
         }
+
         string sentence = sentences.Dequeue(); // Dequeue the next sentence
         StopAllCoroutines(); // Stop any currently running coroutines
         StartCoroutine(TypeSentence(sentence)); // Start typing out the sentence character by character
+
     }
 
-    IEnumerator TypeSentence (string sentence)
+    IEnumerator TypeSentence(string sentence)
     {
         // This coroutine types out the sentence character by character
         dialogueText.text = ""; // Clear the dialogue text
@@ -71,7 +71,6 @@ public class DialogueManager : MonoBehaviour
     void EndDialogue()
     {
         // This method is called when the dialogue ends
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
