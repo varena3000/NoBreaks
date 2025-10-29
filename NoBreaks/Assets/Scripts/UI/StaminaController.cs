4de50480ed2cdd8957807aff3e340c60fc29d939
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2025-10-29 14:44:42 +0200

    Nathan --- Stamina ---
    Changed Stamina values in the script to make it smooth

--- NoBreaks/Assets/Scripts/UI/StaminaController.cs
+++ NoBreaks/Assets/Scripts/UI/StaminaController.cs
@@ -4,23 +4,23 @@ using UnityEngine.UI;
 public class StaminaController : MonoBehaviour
 {
     [Header("Stamina Main Parameters")]
-    [Range(0, 100)]public float playerStamina = 100.0f;
+    [Range(0, 100)]public float playerStamina = 60f;
     [SerializeField]
-    private float maxStamina = 100.0f;
+    private float maxStamina = 60f;
     [HideInInspector]
     public bool hasRegenerated = true;
     [HideInInspector]
     public bool weAreSprinting = false;
 
     [Header("Stamina Regen Parameters")]
-    [Range(0, 50)] [SerializeField] private float staminaDrain = 0.5f;
-    [Range(0, 50)] [SerializeField] private float staminaRegen = 0.5f;
+    [Range(0, 50)] [SerializeField] private float staminaDrain = 60f;
+    [Range(0, 50)] [SerializeField] private float staminaRegen = 20f;
 
     [Header("Stamina Speed Parameters")]
     [SerializeField]
-    private int slowedRunSpeed = 4;
+    private int slowedRunSpeed = 6;
     [SerializeField]
-    private int normalRunSpeed = 8;
+    private int normalRunSpeed = 10;
 
     [Header("Stamina UI Elements")]
     [SerializeField]
