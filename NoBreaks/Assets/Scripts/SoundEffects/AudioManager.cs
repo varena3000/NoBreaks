d1d199d38fccbca9f88cd8489eda3222e33a2e27
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2025-10-31 19:44:57 +0200

    Nathan --- Sound ---
    Added sound clips in the project

--- NoBreaks/Assets/Scripts/SoundEffects/AudioManager.cs
+++ NoBreaks/Assets/Scripts/SoundEffects/AudioManager.cs
@@ -8,6 +8,13 @@ public class AudioManager : MonoBehaviour
 
     [Header("--- Audio  Clip ---")]
     public AudioClip background;
+    public AudioClip Menu;
+    public AudioClip menuButton;
+    public AudioClip Interact;
+    public AudioClip Imbue;
+    public AudioClip Walk;
+    public AudioClip Sprint;
+    public AudioClip Jump;
 
     private void Start()
     {
